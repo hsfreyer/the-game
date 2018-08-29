212748dd2cce43a7e9840f33e415c9093f3a1103
hsfreyer <hans@Hanss-MBP.fritz.box>
2018-08-29 21:47:57 +0200

    Delete merge-code

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,42 +2,6 @@ import React, { Component } from 'react'
 import styled from 'react-emotion'
 
 export default class Card extends Component {
-<<<<<<< HEAD
-  constructor(props) {
-    super(props)
-    this.state = {
-      activePicture: null,
-    }
-  }
-  render() {
-    // console.log(this.state.activePicture)
-    // console.log(this.props.name)
-    const CharacterCard = styled('div')`
-       height: 62vh;
-       width: 20vw;
-       background-position: center;
-       background-size: cover;
-       background-image: url('${
-         this.props.activeCard === this.props.name
-           ? this.props.sketch
-           : this.props.picture
-       }');
-       transform: ${
-         this.props.activeCard === this.props.name
-           ? 'rotate(5deg) scale(1.2) translate(2%, 5%);'
-           : 'rotate(0deg)'
-       };
-      
-       
-       
-      
-`
-    return (
-      <div>
-        <CharacterCard className="characterCard" onClick={this.props.onClick} />
-      </div>
-    )
-=======
   autoplayCard = () => {
     const {
       picture,
@@ -81,6 +45,5 @@ export default class Card extends Component {
   }
   render() {
     return <div>{this.autoplayCard()}</div>
->>>>>>> master
   }
 }
--- src/components/CharacterScreen.js
+++ src/components/CharacterScreen.js
@@ -1,20 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'react-emotion'
-<<<<<<< HEAD
 
-import backgroundImage from '../images/T_CharacterMenu_background.png'
-import TEasy from '../images/T_cm_b1_easy.png'
-import TLinn from '../images/T_cm_b2_linn.png'
-import TBaby from '../images/T_cm_b3_baby.png'
-import TBottle from '../images/T_cm_b4_bottle.png'
-import BtnStart from '../images/bttn_start_on.svg'
-import BtnStartActive from '../images/bttn_start_press.svg'
-
-import SketchEasy from '../images/T_cm_b1_illu_easy.svg'
-import SketchLinn from '../images/T_cm_b2_illu_linn.svg'
-import SketchBaby from '../images/T_cm_b3_illu_baby.svg'
-import SketchBottle from '../images/T_cm_b4_illu_bottle.svg'
-=======
 import { Link } from 'react-router-dom'
 
 import smartphone from '../images/smartphone.svg'
@@ -22,7 +8,7 @@ import smartphone from '../images/smartphone.svg'
 import backgroundImage from '../images/char-sel/T_CharacterMenu_background.jpg'
 import BtnStart from '../images/char-sel/bttn_start_on.svg'
 // import BtnStartPress from '../images/bttn_start_press.svg'
->>>>>>> master
+
 import Card from './Card'
 
 import imgEasy from '../images/char-sel/T_cm_b1_easy.jpg'
@@ -100,61 +86,6 @@ const SecondWrapper = styled('div')`
 `
 
 export default class CharacterScreen extends Component {
-<<<<<<< HEAD
-  constructor(props) {
-    super(props)
-    this.state = {
-      activePic: null,
-      btnActive: false,
-    }
-  }
-  render() {
-    console.log(this.state)
-    return (
-      <Container>
-        <CardWrapper>
-          <Card
-            picture={TEasy}
-            sketch={SketchEasy}
-            name="Easy"
-            key="1"
-            onClick={e => this.setState({ activePic: 'Easy' })}
-            activeCard={this.state.activePic}
-          />
-          <Card
-            picture={TLinn}
-            sketch={SketchLinn}
-            name="Linn"
-            key="2"
-            onClick={e => this.setState({ activePic: 'Linn' })}
-            activeCard={this.state.activePic}
-          />
-          <Card
-            picture={TBaby}
-            sketch={SketchBaby}
-            name="Baby"
-            key="3"
-            onClick={e => this.setState({ activePic: 'Baby' })}
-            activeCard={this.state.activePic}
-          />
-          <Card
-            picture={TBottle}
-            sketch={SketchBottle}
-            name="Bottle"
-            key="4"
-            onClick={e => this.setState({ activePic: 'Bottle' })}
-            activeCard={this.state.activePic}
-          />
-        </CardWrapper>
-        <ImageWrapper>
-          <Image
-            src={this.state.btnActive ? `${BtnStart}` : `${BtnStartActive}`}
-            alt=""
-            onClick={e => this.setState({ btnActive: true })}
-          />
-        </ImageWrapper>
-      </Container>
-=======
   componentDidMount() {
     this.props.resetCharacterScreen()
   }
@@ -235,7 +166,6 @@ export default class CharacterScreen extends Component {
           <img src={smartphone} alt="" />
         </SecondWrapper>
       </div>
->>>>>>> master
     )
   }
 }
