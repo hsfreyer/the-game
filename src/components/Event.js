bfa63bd3b6aa17279d978b795304091ebdd45586
hsfreyer <hans@Hanss-MBP.fritz.box>
2018-08-30 09:24:59 +0200

    Add clickBlock, Styling and Audio for Events

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react'
 import styled from 'react-emotion'
 
+import btnImg from '../images/in-game/bttn_continue_on.svg'
+
 export default class Event extends Component {
   render() {
-    const StyledEvent = styled('img')`
+    const StyledEvent = styled('div')`
       width: 60%;
+      height: 80%;
       position: absolute;
       top: 10%;
       left: 20%;
+      background: url('${this.props.img}') no-repeat center center;
+      background-size: contain;
+    `
+    const ButtonContinue = styled('img')`
+      display: ${this.props.display};
+      width: 20%;
+      height: 20%;
+      transform: rotate(12deg);
+      position: relative;
+      top: 60%;
+      left: 80%;
     `
 
     return (
-      <div>
-        <StyledEvent src={this.props.img} alt="" />
-        <audio src={this.props.audio} autoPlay="false" />
-      </div>
+      <StyledEvent>
+        <ButtonContinue src={btnImg} onClick={this.props.onClick} />
+        <audio src={this.props.audio} autoPlay />
+      </StyledEvent>
     )
   }
 }
