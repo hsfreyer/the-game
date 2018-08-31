a9d37673f8b20eb3b9f246f29fee10b3ff455dfa
hsfreyer <hans@Hanss-MBP.fritz.box>
2018-08-31 21:05:15 +0200

    Center CountDown

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -3,14 +3,25 @@ import styled from 'react-emotion'
 
 export default class CountDown extends Component {
   render() {
-    const StyledCountDown = styled('img')`
+    const CountWrapper = styled('div')`
       height: 25%;
+      width: 50%;
+      display: flex;
+      justify-content: space-around;
       position: absolute;
       bottom: 50%;
-      left: 40%;
+      left: 25%;
       z-index: 10;
     `
 
-    return <StyledCountDown src={this.props.img} />
+    const StyledCountDown = styled('img')`
+      height: 100%;
+    `
+
+    return (
+      <CountWrapper>
+        <StyledCountDown src={this.props.img} />
+      </CountWrapper>
+    )
   }
 }
