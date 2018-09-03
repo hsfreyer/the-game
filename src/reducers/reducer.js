08650efac8e9378f76d2ff152aa26ca86e02b555
hsfreyer <hans@Hanss-MacBook-Pro.local>
2018-09-03 17:54:09 +0200

    Revert "Fix continue-button-bug"
    This reverts commit 4ce5d0be0a20fb586abe47e82188bf18f89fc32d.

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -108,7 +108,6 @@ export default (state, action) => {
     case Actions.RESET_CHARACTERSCREEN:
       return {
         ...state,
-        isEvent: false,
         player: {
           tile: 0,
           band: null,
